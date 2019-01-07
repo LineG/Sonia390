4976b04c44cf05097572b55583ca2d4831a20b64
Charles <chanc09@gmail.com>
2019-01-07 17:31:57 -0500

    setup original app

---
+++ app/k9mail/build/generated/source/buildConfig/androidTest/debug/com/fsck/k9/tests/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.fsck.k9.tests;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.fsck.k9.tests";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 27000;
+  public static final String VERSION_NAME = "5.700-SNAPSHOT";
+  // Fields from build type: debug
+  public static final boolean DEVELOPER_MODE = true;
+}
