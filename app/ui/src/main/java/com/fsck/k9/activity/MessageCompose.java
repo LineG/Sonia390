4f96765f4ba1920ca8abb0b93aa6a222e7781ac4
LineG <ghanemline@gmail.com>
2019-02-01 19:05:45 -0500

    #13 fixed some issues for codacy (second time)

--- app/ui/src/main/java/com/fsck/k9/activity/MessageCompose.java
+++ app/ui/src/main/java/com/fsck/k9/activity/MessageCompose.java
@@ -335,22 +335,23 @@ public class MessageCompose extends K9Activity implements OnClickListener,
                                                 getResources().getStringArray(R.array.message_compose_templates));
         templatesAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         templatesSpinnerView.setAdapter(templatesAdapter);
-        greetingText ="";
+        greetingText = "";
         templatesSpinnerView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(position == 0) {
-                    greetingText= "";
+                    greetingText = "";
                 }
                 else if(position == 1) {
-                    greetingText= "Dear Dr.(last name),\n\nMy name is <<name>> and my student ID is (student id). I am sending this message in regards to (inquiry)"
+                    greetingText = "Dear Dr.(last name),\n\nMy name is <<name>> and my student ID is (student id)."
+                            + " I am sending this message in regards to (inquiry)"
                     + "\n\n" + "I hope to hear from you soon. Thank you  very much for your time.\n\n Best regard,\n\n(name)";
                 }
                 else if(position == 2) {
-                    greetingText= "Boss";
+                    greetingText = "Boss";
                 }
                 else if(position == 3) {
-                    greetingText= "(month, day year>>\n\naddress)\n\n" +
+                    greetingText = "(month, day year>>\n\naddress)\n\n" +
                             "Dear Mr.(last name),\n\nI am writing to apply for the (position) advertised in the (advertisement). " +
                             "I enclosed a completed job application, my certification, my resume and (number) references.\n\n" +
                             "The role is very appealing to me, and I believe that my strong technical experience and education" +
@@ -359,35 +360,36 @@ public class MessageCompose extends K9Activity implements OnClickListener,
                             "With a (academic degree), I have a comprehensive understanding of this domain. I also have experience in (experiences)."+
                             " Please see my resume for additional information on my experience.\n\n " +
                             "I can be reached anytime via email at (email) or by cell phone, (number).\n\n" +
-                            "Thank you for your time and consideration. I look forward to speaking with you about this employment opportunity.\n\nSincerely,\n\n(name)";
+                            "Thank you for your time and consideration." +
+                            " I look forward to speaking with you about this employment opportunity.\n\nSincerely,\n\n(name)";
                 }
                 else if(position == 4) {
-                    greetingText=
+                    greetingText =
                             "Dear Mr/Ms.(last name)\n\n" +
                             "My name is (first and last name) and this letter is in reference to vacancy at your residency at (address)."+
                                     " I am (academic degree) from a reputable institution.\n" +
                             "I would like to inquire about the conditions for rental and the monthly rental fee.\n\n " +
-                            "I am looking for a place to stay at least (number) years as I take my (studies/job)."+
+                            "I am looking for a place to stay at least (number) years as I take my (studies/job)." +
                                     " I would be checking out your (apartment/house) as soon as I receive a positive reply from you. " +
                                     "Please send your reply via email or you may contact me at (number). Thank you for your time\n\n" +
                             "Sincerely,\n\n" +
                             "(name)";
                 }
                 else if(position == 5) {
-                    greetingText= "Friends";
+                    greetingText = "Friends";
                 }
                 else if(position == 6) {
-                    greetingText= "Dear (name)\n\nPlease mark your calendars for the (Insert Event Name)\n\nDate:(Insert Date)\n" +
-                            "Time:(Insert Time)\nLocation:(Insert Location)\n\n"+
+                    greetingText = "Dear (name)\n\nPlease mark your calendars for the (Insert Event Name)\n\nDate:(Insert Date)\n" +
+                            "Time:(Insert Time)\nLocation:(Insert Location)\n\n" +
                             "(Insert ant details including what they need to bring, if anything, and " +
-                            "if siblings/parents/grade level kids only are invited, etc.)"+
+                            "if siblings/parents/grade level kids only are invited, etc.)" +
                             "\n\nPlease RSVP no later than (insert RSVP by date) by replying" +
-                            "to this message, calling me at (number), or emailing me at (email)"+
+                            "to this message, calling me at (number), or emailing me at (email)" +
                             "\n\n(Insert any other personalizations)\n\n" +
                             "I hope to see you all there,\n\n(Insert name of PA rep or event sponsor)\n\nSincerely,\n\n(name).";
                 }
                 else if(position == 7) {
-                    greetingText= "Children";
+                    greetingText = "Children";
                 }
 
             }
