e690caa46bca17785d82e756a5ceebe8d9937e0d
LineG <ghanemline@gmail.com>
2019-04-04 16:13:28 -0400

    #78 fixed codacy

--- k9mail/src/main/java/com/fsck/k9/fragment/ContactHolder.java
+++ k9mail/src/main/java/com/fsck/k9/fragment/ContactHolder.java
@@ -2,7 +2,6 @@ package com.fsck.k9.fragment;
 
 import android.widget.TextView;
 
-import com.fsck.k9.mail.internet.Viewable;
 
 public class ContactHolder {
 
--- k9mail/src/main/java/com/fsck/k9/fragment/ContactListAdapter.java
+++ k9mail/src/main/java/com/fsck/k9/fragment/ContactListAdapter.java
@@ -1,16 +1,13 @@
 package com.fsck.k9.fragment;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
-import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.fsck.k9.R;
-import com.fsck.k9.activity.ContactList;
 import com.fsck.k9.firebasedb.Contact;
 
 import java.util.ArrayList;
@@ -23,8 +20,8 @@ public class ContactListAdapter extends ArrayAdapter<Contact> {
     private List<Contact> contactList;
 
 
-    public ContactListAdapter(@NonNull Context context, ArrayList<Contact> list){
-        super(context, 0 , list);
+    public ContactListAdapter(@NonNull Context context, ArrayList<Contact> list) {
+        super(context, 0, list);
         mContext = context;
         contactList = list;
     }
@@ -36,7 +33,7 @@ public class ContactListAdapter extends ArrayAdapter<Contact> {
 
         ContactHolder holder = new ContactHolder();
 
-        if (convertView == null){
+        if (convertView == null) {
 
             convertView = LayoutInflater.from(mContext).inflate(R.layout.adapter_contact_list_view,
                     null);
@@ -47,7 +44,7 @@ public class ContactListAdapter extends ArrayAdapter<Contact> {
         }
 
         Contact contact = getItem(position);
-        String name = contact.getName()+" "+contact.getLastName();
+        String name = contact.getName() + " " + contact.getLastName();
         holder.contactName.setText(name);
         holder.contactEmail.setText(contact.getEmail());
 
